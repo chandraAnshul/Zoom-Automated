d02783fdbfb289515abc0f9b9e357e1c01104526
chandraAnshul <91283625+chandraAnshul@users.noreply.github.com>
2021-09-23 23:44:24 +0530

    Add files via upload

---
+++ Zoom_2.py
@@ -0,0 +1,53 @@
+import pyautogui as auto
+import time
+from datetime import datetime
+lst = [['2128248039','PeaceTalk','22:58','23:00']]
+isStarted = False
+
+auto.press("win")
+time.sleep(4)
+auto.press('z')
+time.sleep(4)
+auto.press('enter')
+time.sleep(4)
+for i in range(7):
+    auto.press('tab')
+# auto.press('enter')
+
+time.sleep(2)
+for l in lst:
+    while True:
+        if isStarted == False:
+            if datetime.now().hour == int(l[2].split(':')[0]) and datetime.now().minute == int(l[2].split(':')[1]):
+                auto.press('enter')
+                auto.press('ab')
+                time.sleep(2)
+                for i in l[0]:
+                    auto.press(i)
+                auto.press('enter')
+                time.sleep(6)
+                auto.press('enter')
+                auto.press('ab')
+                time.sleep(2)
+                for i in l[0]:
+                    auto.press(i)
+                time.sleep(3)
+                auto.press('enter')
+                isStarted = True
+        elif isStarted == True:
+            if datetime.now().hour == int(l[3].split(':')[0]) and datetime.now().minute == int(l[3].split(':')[1]):
+                auto.press("enter")                         
+                auto.keyDown('alt')
+                auto.press('q')
+                auto.keyUp('alt')
+                isStarted = False
+                break
+
+time.sleep(5)
+for i in range(5):
+    auto.press('tab')
+
+    
+    
+    
+    
